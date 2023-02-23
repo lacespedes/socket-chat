e995b8f28e529729a8039c43c2702364d8b07da1
Leonardo Cespedes <leonardo.cespedes@gmail.com>
2023-02-22 22:58:06 -0500

    Primer commit aplicacion de chat

---
+++ public/js/socket-chat.js
@@ -0,0 +1,49 @@
+const socket = io();
+
+const params = new URLSearchParams(window.location.search);
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
+}
+
+const usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', () => {
+    console.log('Conectado al servidor');
+    socket.emit('entrarChat', usuario, ( resp ) => {
+        console.log('Usuarios conectados ', resp);
+    });
+    
+});
+
+// escuchar
+socket.on('disconnect', () => {
+    console.log('Perdimos conexión con el servidor');
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, (resp) => {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', (mensaje) => {
+    console.log('Servidor:', mensaje);
+});
+ 
+// Escuchar información
+socket.on('listaPersona', (personas) => {
+    console.log('Servidor:', personas);
+});
+
+// mensakes privados
+socket.on('mensajePrivado', (mensaje) => {
+    console.log('Mensaje Privado:', mensaje);
+});
